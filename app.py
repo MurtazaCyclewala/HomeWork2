7340ea0d0f97f53a22639a87c91de7f3cd983284
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-26 15:05:02 +0300

    Entered A profile page and css file for it

--- app.py
+++ app.py
@@ -15,8 +15,17 @@ def index():
     return render_template('Login.html')
 
 
-@app.route('/login')
+@app.route('/login', methods = ['POST'])
 def login():
+    users = mongo.db.users
+    login_users = users.find_one({'name' : request.form['uname']})
+    if login_users:
+        if request.form['psw'] == login_users['password']:
+            session['username'] = request.form['uname']
+            return redirect(url_for('index'))
+        return render_template('Login.html',error="Invalid Username/Password")
+    return render_template('Login.html',error="Invalid Username/Password Combination")
+
     return render_template("Login.html")
 
 @app.route('/signup', methods = ['POST','GET'])
@@ -26,14 +35,16 @@ def signup():
         existing_user = users.find_one({'name' : request.form['username']})
 
         if existing_user is None:
-            hashpass = request.form['psw']
-            users.insert_one( {'name' : request.form['username'], 'password' : hashpass,
-            'FirstName' : request.form['fname'], 'SecondName' : request.form['ffname'],
-            'ThirdName' : request.form['tname'], 'Birthday' : request.form['birthday'],
-            'FOI' : request.form['field'], 'Gender' : request.form['gender']} )
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        return "That username exists"
+            if request.form['psw'] == request.form['cpsw']:
+                hashpass = request.form['psw']
+                users.insert_one( {'name' : request.form['username'], 'password' : hashpass,
+                'FirstName' : request.form['fname'], 'SecondName' : request.form['ffname'],
+                'ThirdName' : request.form['tname'], 'Birthday' : request.form['birthday'],
+                'FOI' : request.form['field'], 'Gender' : request.form['gender']} )
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            return render_template('SignUp.html',error="Please Enter Same password")
+        return render_template('SignUp.html',error="That Username Exists")
     
     return render_template('SignUp.html')
 
